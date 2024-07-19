7e4c2f0a78ccb5c460852e58c027bf811eea9626
Vinicius <viniciusbsilva57@gmail.com>
2024-07-19 12:44:33 -0300

    fix sizes

--- src/app/(paginas)/page.tsx
+++ src/app/(paginas)/page.tsx
@@ -27,7 +27,7 @@ export default async function Home() {
               priority
               quality={100}
               className="object-cover object-center -z-10"
-              sizes="(max-width: 1024px) 50vw, (max-width: 1920px) 100vw, 100vw"
+              sizes="(max-width: 360px) 100vw, (max-width: 1920px) 100vw, 100vw"
             />
           </div>
           <div className="absolute bottom-0 px-[12%]">
