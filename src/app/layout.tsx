7351638480e5e739e56e963ea8c6bd6d09973d98
Vinícius Bastos <viniciusbsilva57@gmail.com>
2024-11-08 22:37:35 -0300

    feat: add search console configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Manrope } from "next/font/google";
 import Footer from "@/components/Footer";
+import "swiper/swiper-bundle.css";
+
+import SmoothScroll from "@/components/SmoothScroll";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -12,10 +15,18 @@ const manrope = Manrope({
 export const metadata: Metadata = {
   title: "The Movie Dictionary",
   description: "Descubra informações detalhadas sobre filmes e séries.",
-  keywords:
-    "filmes, séries, informações sobre filmes, dicionário de filmes, sinpose, elenco, informações",
+  keywords: [
+    "filmes",
+    "séries",
+    "informações sobre filmes",
+    "dicionários de filmes",
+    "sinopse",
+    "elenco",
+    "informações",
+    "The Movie Dictionary",
+    "MovieDictionary",
+  ],
   creator: "Vinícius Bastos",
-  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
@@ -25,10 +36,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={manrope.className}>
-        {children}
-        <Footer />
-      </body>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="xPNE8_Rgq3mxaABEFT0rPd3WSofE7ZOwU4D1zCO7Jok"
+        />
+      </head>
+      <SmoothScroll>
+        <body className={manrope.className}>
+          {children}
+          <Footer />
+        </body>
+      </SmoothScroll>
     </html>
   );
 }
